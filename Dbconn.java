1796dd648b1e556b209c790949fc6bb73c25e782
PranavNag2005 <uppalapatipranavnag@gmail.com>
2025-05-30 12:02:07 +0530

    updated code

--- Dbconn.java
+++ Dbconn.java
@@ -5,12 +5,21 @@ private static final String user="root";
 private static final String pass="root";
 
 public static Connection getConnection(){
-    try{
-        return DriverManager.getConnection(url,user,pass);
+    Connection conn=null;
+        try{
+            conn=DriverManager.getConnection(url,user,pass);
+        return conn;
     }
     catch(SQLException e){
-    System.out.println("Database connection failed");
+    System.out.println("Database connection failed" );
     return null;
+    }
+}
+public static void main(String[] args){
+    Connection db=getConnection();
+    System.out.println(db);
+    if(db!=null){
+        System.out.println("connection successfull");
     }
 }
 
